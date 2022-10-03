bee2491aa565691006456f20c921b4cfbf383948
Alex <alexander.tschanun@icloud.com>
2022-10-03 12:24:22 +0200

    Bankomat_finished

--- src/Bankomat.java
+++ src/Bankomat.java
@@ -5,31 +5,33 @@ public class Bankomat
 {
     public static void main(String[] args)
     {
-        System.out.println("1. Einzahlen" + "\n" + "2. Auszahlen" + "\n" + "3. Kontostand" + "\n" + "4. Beenden");
-        Scanner scanner = new Scanner(System.in);
-        int selection = scanner.nextInt();
-        Scanner betrag = new Scanner(System.in);
-        double moneysum = betrag.nextDouble();
 
-        do {
+        boolean isFinished = false;
+        double balance = 0;
+
+        while(isFinished == false) {
+            System.out.println("1. Einzahlen" + "\n" + "2. Auszahlen" + "\n" + "3. Kontostand" + "\n" + "4. Beenden");
+            Scanner scanner = new Scanner(System.in);
+            int selection = scanner.nextInt();
             switch (selection) {
                 case 1:
-
-                    System.out.println(1);
+                    double deposited = scanner.nextDouble();
+                    balance = balance + deposited;
+                    System.out.println("Successfully added " + deposited + "€");
                     break;
                 case 2:
-
-                    System.out.println(2);
+                    double subtracted = scanner.nextDouble();
+                    balance = balance - subtracted;
+                    System.out.println("Successfully subtracted " + subtracted + "€");
                     break;
                 case 3:
-
-                    System.out.println(3);
+                    System.out.println("Your current balance is " + balance + "€");
                     break;
                 case 4:
-
-                    System.out.println(4);
+                    isFinished = true;
+                    System.out.println("Please come back anytime");
                     break;
             }
-        }while(selection != 4);
+        }
     }
 }
