0374c6e11276ce13eefc3f5c47b174c1511e25d1
Alex <alexander.tschanun@icloud.com>
2022-11-14 13:06:23 +0100

    StringHelper_palindrome_finished

--- src/StringHelper.java
+++ src/StringHelper.java
@@ -6,6 +6,10 @@ public class StringHelper
     {
         Scanner input = new Scanner(System.in);
         boolean isFinished = false;
+        String messageToCheckPalindrome = "";
+        char letter;
+        String messageToCountLetters = "";
+        String messageToReverse = "";
 
         while(!isFinished)
         {
@@ -15,11 +19,29 @@ public class StringHelper
             switch (selection)
             {
                 case 'p':
-                    String encryptedMessage = isPalindrome("placeholder");
+                    System.out.println("Please enter the Message in only small cases");
+                    messageToCheckPalindrome = input.next();
+                    boolean palindromeCheck = isPalindrome(messageToCheckPalindrome);
+                    if (palindromeCheck == false)
+                    {
+                        System.out.println("The Message isn't a Palindrome");
+                    }
+                    else
+                    {
+                        System.out.println("The Message is a Palindrome");
+                    }
                     break;
                 case 'c':
+                    System.out.println("Which Letter is being searched?");
+                    letter = input.next().charAt(0);
+                    System.out.println("Please enter the Message");
+                    messageToCountLetters = input.next();
+                    String countedMessage = countLetters(letter, messageToCountLetters);
                     break;
                 case 'r':
+                    System.out.println("Please enter the Message you wish to be reversed");
+                    messageToReverse = input.next();
+                    String reverseMessage = reversingMessage(messageToReverse);
                     break;
                 case 'a':
                     break;
@@ -29,22 +51,42 @@ public class StringHelper
             }
         }
     }
-    public static String isPalindrome(String palindromeMessage)
+    public static boolean isPalindrome(String messageToCheckPalindrome)
+    {
+        char[] inputMessage = messageToCheckPalindrome.toCharArray();
+        char[] revertedMessage = reversingMessage(messageToCheckPalindrome).toCharArray();
+        boolean palindromeCheck = true;
+        for(int i = 0; i < inputMessage.length; i++)
+        {
+            int currentLetter = inputMessage[i];
+            int currentRevertedLetter = revertedMessage[i];
+
+            if (currentLetter != currentRevertedLetter)
+            {
+                palindromeCheck = false;
+            }
+        }
+        return palindromeCheck;
+
+    }
+    public static String countLetters(char letter, String messageToCountLetters)
     {
 
         return"";
     }
-    public static String countLetters(char letter, String countMessage)
+    public static String reversingMessage(String messageToReverse)
     {
-
-        return"";
+        char[] inputMessage = messageToReverse.toCharArray();
+        char[] resultingMessage = new char[inputMessage.length];
+        int counter = 0;
+        for(int i = inputMessage.length-1; i >=0; i--)
+        {
+            resultingMessage[counter]= inputMessage[i];
+            counter++;
+        }
+        return String.valueOf(resultingMessage);
     }
-    public static String reverseMessage(String messageToReverse)
-    {
-
-        return"";
-    }
-    public static String amountOfLetters(String )
+    public static String amountOfLetters(String messageToCountAmount )
     {
 
         return"";
