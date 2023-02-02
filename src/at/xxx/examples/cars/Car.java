e9c38b37b2e5f342bc1d39cddf2ce74cb88866d6
Alex <alexander.tschanun@icloud.com>
2023-02-02 09:27:16 +0100

    OO_RemoteControl_11_Cars2_innit_02.02.2023

---
+++ src/at/xxx/examples/cars/Car.java
@@ -0,0 +1,14 @@
+package at.xxx.examples.cars;
+
+import OO_Car.Engine;
+
+public class Car {
+    //Gedächtnisvariablen
+
+    private Engine engine;
+    private Seller seller;
+    private int maxSpeed;
+    private int baseConsumption;
+    private double basePrice;
+    private String color;
+}
