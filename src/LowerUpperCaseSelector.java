77b95ab611e2c1bd5a301c93f1e5d51366616ce6
Alex <alexander.tschanun@icloud.com>
2023-02-02 08:49:03 +0100

    LowerUpperCaseSelector_02.02.2023

---
+++ src/LowerUpperCaseSelector.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class LowerUpperCaseSelector {
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        String upperCases = "";
+        String lowerCases = "";
+        String message = "";
+        message = input.next();
+        char[] arr = message.toCharArray();
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            if(Character.isUpperCase(arr[i]))
+            {
+                upperCases = upperCases + (arr[i]);
+            }
+            else
+            {
+                lowerCases = lowerCases + (arr[i]);
+            }
+        }
+        System.out.println(upperCases);
+        System.out.println(lowerCases);
+    }
+
+}
