1b9b709882bb1dd3badd1f6aeddbb7f5772f7ffa
Alex <alexander.tschanun@icloud.com>
2023-03-15 11:05:00 +0100

    OO_14.2_Accounts_finished_15.03.2023

--- src/OO_Account/CreditAccount.java
+++ src/OO_Account/CreditAccount.java
@@ -1,4 +1,34 @@
 package OO_Account;
 
-public class CreditAccount {
+public class CreditAccount extends BaseAccount
+{
+    private int interestRate;
+    //Konstruktor
+    public CreditAccount(String owner, double balance, int interestRate) {
+        super(owner, balance);
+        this.interestRate = interestRate;
+    }
+    //Funktionen
+    @Override
+    public void deposit(double amount){
+        if(0 >= this.getBalance() + amount)
+        {
+            System.out.println("Cleared " + amount + "€ from the credit");
+            this.setBalance(getBalance() + amount);
+        }
+        else
+        {
+            System.out.println("Credit can't be positive");
+        }
+
+    }
+
+    //Getter & Setter
+
+    public int getInterestRate() {
+        return interestRate;
+    }
+    public void setInterestRate(int interestRate) {
+        this.interestRate = interestRate;
+    }
 }
--- src/OO_Account/Main.java
+++ src/OO_Account/Main.java
@@ -6,8 +6,11 @@ public class Main
     {
         SavingsAccount sa1 = new SavingsAccount("Hannes", 500.00);
         CheckingsAccount ca1 = new CheckingsAccount("Finn", 350.00, 200.00);
+        CreditAccount cra1 = new CreditAccount("Leonie", -350, 2);
 
         sa1.withdraw(500);
         ca1.withdraw(560);
+        cra1.deposit(20);
+        System.out.println(cra1.getOwner());
     }
 }
